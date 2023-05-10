720cc3efc4bdab4e6ed6b9446e43f4a1de28fb27
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-10 14:15:53 +0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,10 @@ class BaseModel():
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.now().strftime(%Y-%m-%dT%H:%M:%S.%f)
+        self.updated_at = datetime.now().strftime(%Y-%m-%dT%H:%M:%S.%f)
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now().strftime(%Y-%m-%dT%H:%M:%S.%f)
     def to_dict(self):
     def __str__(self):
           """
