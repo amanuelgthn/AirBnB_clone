1a04784b9f9deb49e0e4f9dee16f36fa68ba39f1
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-10 11:56:04 +0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,3 +15,7 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+    def save(self):
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        
