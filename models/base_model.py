aa4f45a13fa21d80873f4428e192d0403394b1b2
amanuelgthn <amanuelgthn@gmail.com>
2023-05-12 16:07:29 +0800

    Update to create BaseModel from Dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,24 +11,49 @@ class BaseModel:
     """
 
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            id_found = False
+            created_found = False
+            updated_found = False
+            for key,value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                    id_found = True
+                if key == "created_at":
+                    self.created_at = value
+                    created_found = True
+                if key == "updated_at":
+                    self.updated_at = value
+                    updated_found = True
+        if id_found == False:
+            self.id = str(uuid.uuid4())
+        if created_found == False:
+            self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        
+
+    @property
+    def get_id(self):
+        return self.id
+    
     def save(self):
         self.updated_at = datetime.datetime.now()
         
-    def to_dict(self):
-        return BaseModel.__dict__
-    
+    def to_dict(obj):
+        public_attr = {}
+        for attr in obj.__dict__:
+            if not attr.startswith("__") and type(attr) != "<class 'method'>":
+                public_attr[attr] = getattr(obj,attr)
+        public_attr["__class__"] = str(type(obj).__name__)
+        return public_attr
     def __str__(self):
         """
         __str__ method
         """
         result = ""
         result = "[" + str(type(self).__name__) + "] " + "("
-        result += str(self.id) + ") "
-        result += str(self.__dict__)
+        result += str(self.get_id) + ") "
+        dict_attr = self.to_dict()
+        dict_attr.pop("__class__")
+        result += str(dict_attr)
         return result
-        
