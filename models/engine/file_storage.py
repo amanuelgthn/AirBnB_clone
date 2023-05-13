9f6090cb5b28ca046cc40870c2c0cdccb7f43422
amanuelgthn <amanuelgthn@gmail.com>
2023-05-13 08:50:19 +0800

    Update Progress

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that contains FileStorage that serializes instances to a JSON file
+and deserializes JSON file to instances
+"""
+import json
+import uuid
+import datetime
+
+class FileStorage:
+    """
+    FileStorage Class
+    """
+    
