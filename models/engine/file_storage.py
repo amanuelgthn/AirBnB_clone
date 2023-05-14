27c7912bd25222fa40565400ca558c667f2d0611
amanuelgthn <amanuelgthn@gmail.com>
2023-05-14 15:39:49 +0800

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ and deserializes JSON file to instances
 """
 from models.base_model import BaseModel
 import json
+dict_ref = {"BaseModel": BaseModel}
 
 class FileStorage():
     """
@@ -25,13 +26,22 @@ class FileStorage():
         file_dict = {}
         for key in self.__objects.keys():
             file_dict[key] = self.__objects[key].to_dict()
-        with open(self.__file_path,'w', encoding="utf-8") as file:
+        with open(self.__file_path,"w+", encoding="utf-8") as file:
             json.dump(file_dict,file)
+    #def reload(self):
+       # try:
+           # with open(self.__file_path,'r',encoding="utf-8") as file:
+            ##    objects = json.load(file)
+               # for key in objects:
+               #     self.__objects[key] = dict_ref[objects[key]["__class__"]](**objects[key])
+       # except FileNotFoundError:
+           # pass
     def reload(self):
+        """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path,'r',encoding="utf-8") as file:
-                objects = json.load(file)
-                #for key in objects:
-                    #self.__objects[key] =objects[key]
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
             pass
