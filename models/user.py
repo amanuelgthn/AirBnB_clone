a472d7300d36950578f7f6456113a47ecf842211
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-15 08:21:39 +0800

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that contains User Class that inherits from BaseModel
+"""
+
+
+import uuid
+import datetime
+from basemodel
+
+
+class User(BaseModel):
+    """
+    User Class
+    """
