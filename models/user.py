bed025377037e9f6448afaaeca85729f8fcd2720
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-15 08:32:10 +0800

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,13 +3,21 @@
 Module that contains User Class that inherits from BaseModel
 """
 
-
-import uuid
-import datetime
-from basemodel
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """
     User Class
     """
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization
+        """
+        super().__init__(self, *args, **kwargs)
