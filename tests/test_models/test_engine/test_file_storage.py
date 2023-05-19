59f7c5e823b98adb33e10c9f06d42ae13cf3441a
Amanuel <amanuel@topformbras.com.cn>
2023-05-19 16:39:41 +0800

    Update Test_models

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,10 +6,16 @@ Module to test file_storage.py module
 
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class FileStorage_testcase(unittest.TestCase):
     """
     test case for FileStorage
     """
-    
+    def test_file_path(self):
+        base = BaseModel()
+        self.assertEqual(base.__file_path, "file.json")
+
+if __name__ == "__main__":
+    unittest.main()
