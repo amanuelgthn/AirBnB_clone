f1e7645d12fd598bf7baa76bfedf7903f4619201
Amanuel <amanuel@topformbras.com.cn>
2023-05-19 16:18:45 +0800

    create Test_file_storage.py

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to test file_storage.py module
+"""
+
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class FileStorage_testcase(unittest.TestCase):
+    """
+    test case for FileStorage
+    """
+    
