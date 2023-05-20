32065aa61c7f63142ac85a70b0b4b9bccaa19b00
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 11:02:17 +0800

    update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -15,13 +15,9 @@ class User_testcase(unittest.TestCase):
     """
     def test_first_name(self):
         """
-        test for public class attributes
+        test for first_name
         """
         my_user = User()
-        my_user.first_name = "Betty"
-        my_user.last_name = "Bar"
-        my_user.email = "airbnb@mail.com"
-        my_user.password = "root"
         my_user.save()
         my_user2 = User()
         my_user2.first_name = "John"
@@ -30,10 +26,13 @@ class User_testcase(unittest.TestCase):
         my_user2.password = "root2"
         my_user2.save()
         self.assertTrue(hasattr(my_user, "first_name"))
-        self.assertEqual(my_user.first_name, "Betty")
-        self.assertEqual(my_user.last_name, "Bar")
-        self.assertEqual(my_user.email, "airbnb@mail.com")
-        self.assertEqual(my_user.password, "root")
+        self.assertEqual(my_user.first_name, "")
+        self.assertTrue(hasattr(my_user, "last_name"))
+        self.assertEqual(my_user.last_name, "")
+        self.assertEqual(my_user.email, "")
+        self.assertTrue(hasattr(my_user, "email"))
+        self.assertEqual(my_user.password, "")
+        self.assertTrue(hasattr(my_user, "password"))
         self.assertEqual(my_user2.first_name, "John")
         self.assertEqual(my_user2.last_name, "Phalange")
         self.assertEqual(my_user2.email, "airbnb2@mail.com")
