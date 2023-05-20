447ccf53225e26039b1adaa7cf0a514ef3c6dcb1
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 09:48:26 +0800

    Update Test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,8 @@ Module to test file_storage.py module
 
 
 import unittest
+from datetime import datetime
+from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
@@ -14,9 +16,16 @@ class FileStorage_testcase(unittest.TestCase):
     test case for FileStorage
     """
     def test_file_path(self):
-        my_model = FileStorage()
+        my_model = BaseModel(BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572"))
         my_model.save()
-        self.assertEqual(my_model.file_path, "file.json")
+        all_objs = storage.all()
+        for obj_id in all_objs.keys():
+            if obj_id == "id":
+                self.assertEqual(all_objs[id], 'b6a6e15c-c67d-4312-9a75-9d084935e579')
+            if obj_id == "created_at":
+                self.assertEqual(all_objs[created_at], datetime(2017, 9, 28, 21, 5, 54, 119572))
+            if obj_id == "updated_at":
+                self.assertEqual(all_objs[updated_at], datetime(2017, 9, 28, 21, 5, 54, 119572))
 
 if __name__ == "__main__":
     unittest.main()
