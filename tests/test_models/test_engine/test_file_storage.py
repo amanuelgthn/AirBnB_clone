bad75daf91457ed05c6c53b81b22677cb84ed37b
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 10:02:09 +0800

    Update Models

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,7 +16,7 @@ class FileStorage_testcase(unittest.TestCase):
     test case for FileStorage
     """
     def test_file_path(self):
-        my_model = BaseModel(BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572"))
+        my_model = BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572")
         my_model.save()
         all_objs = storage.all()
         for obj_id in all_objs.keys():
