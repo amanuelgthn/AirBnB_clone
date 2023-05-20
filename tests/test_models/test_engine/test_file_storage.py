b805e3a15853609f1c1e0991050597326de6bc82
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 14:18:11 +0800

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,28 +9,19 @@ from datetime import datetime
 from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage_testcase(unittest.TestCase):
     """
     test case for FileStorage
     """
-    def test_file_path(self):
-        my_model = BaseModel(id='b6a6e15c-c67d-4312-9a75-9d084935e579',
-                             created_at="2017-09-28T21:05:54.119572",
-                             updated_at="2017-09-28T21:05:54.119572")
-        my_model.save()
-        all_objs = storage.all()
-        for obj_id in all_objs.keys():
-            if obj_id == "id":
-                self.assertEqual(all_objs[id],
-                                 'b6a6e15c-c67d-4312-9a75-9d084935e579')
-            if obj_id == "created_at":
-                self.assertEqual(all_objs[created_at],
-                                 datetime(2017, 9, 28, 21, 5, 54, 119572))
-            if obj_id == "updated_at":
-                self.assertEqual(all_objs[updated_at],
-                                 datetime(2017, 9, 28, 21, 5, 54, 119572))
+    
+    
+    def test_all(self):
+        model = User()
+        model.storage = FileStorage()
+        self.assertIsNotNone(model.storage.all())
 
 
 if __name__ == "__main__":
