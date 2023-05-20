7cb87437f94a4f32455a24fa0b2a704609d68677
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 10:32:56 +0800

    Update test_base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def save(self):
