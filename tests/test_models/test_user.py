dd87f77ccb85f47e44bf21f79888071961986569
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 10:46:23 +0800

    update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -17,14 +17,27 @@ class User_testcase(unittest.TestCase):
         """
         test for public class attributes
         """
-        User.first_name = "Betty"
-        User.last_name = "Bar"
-        User.email = "airbnb@mail.com"
-        User.password = "root"
-        self.assertEqual(User.first_name, 'Betty')
-        self.assertEqual(User.last_name, "Bar")
-        self.assertEqual(User.email, "airbnb@mail.com")
-        self.assertEqual(User.password, "root")
+        my_user = User()
+        my_user.first_name = "Betty"
+        my_user.last_name = "Bar"
+        my_user.email = "airbnb@mail.com"
+        my_user.password = "root"
+        my_user.save()
+        my_user2 = User()
+        my_user2.first_name = "John"
+        my_user2.email = "airbnb2@mail.com"
+        my_user2.last_name = "Phalange"
+        my_user2.password = "root2"
+        my_user2.save()
+        self.assertEqual(my_user.first_name, "Betty")
+        self.assertEqual(my_user.last_name, "Bar")
+        self.assertEqual(my_user.email, "airbnb@mail.com")
+        self.assertEqual(my_user.password, "root")
+        self.assertEqual(my_user2.first_name, "John")
+        self.assertEqual(my_user2.last_name, "Phalange")
+        self.assertEqual(my_user2.email, "airbnb2@mail.com")
+        self.assertEqual(my_user2.password, "root2")
 
 if __name__ == "__main__":
     unittest.main()
+    
