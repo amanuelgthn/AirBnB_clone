11a3103e683e0475ae6795c0bb968c3eb594b310
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 10:23:48 +0800

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@ Module to unittest the base_model.py module
 """
 
 
+from models import storage
 from datetime import datetime
 from uuid import uuid4
 import unittest
@@ -24,13 +25,26 @@ class BaseModel_TestCase(unittest.TestCase):
         self.assertEqual(base.id, 'b6a6e15c-c67d-4312-9a75-9d084935e579')
   
     def test_to_dict(self):
-        base = BaseModel()
-        self.assertEqual(type(base.to_dict()),dict)
+        base = BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572")
+        base_dict = base.to_dict()
+        self.assertEqual(base_dict, {'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T21:05:54.119572', 'updated_at': '2017-09-28T21:05:54.119572', '__class__': 'BaseModel'})
 
     def test__str__(self):
         base = BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572")
         base_print = base.__str__()
         self.assertEqual(base_print,"[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572), 'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572)}")
+
+    def test_save(self):
+        my_model = BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572")
+        my_model.save()
+        all_objs = storage.all()
+        for obj_id in all_objs.keys():
+            if obj_id == "id":
+                self.assertEqual(all_objs[id], 'b6a6e15c-c67d-4312-9a75-9d084935e579')
+            if obj_id == "created_at":
+                self.assertEqual(all_objs[created_at], datetime(2017, 9, 28, 21, 5, 54, 119572))
+            if obj_id == "updated_at":
+                self.assertEqual(all_objs[updated_at], datetime(2017, 9, 28, 21, 5, 54, 119572))
 
 if __name__ == "__main__":
     unittest.main()
