aa983f7ca0be97376dafa0bcf7de2981cbd21008
amanuelgthn <amanuelgthn@gmail.com>
2023-05-20 10:17:33 +0800

    Update test_baseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -22,8 +22,15 @@ class BaseModel_TestCase(unittest.TestCase):
         """ with arguments"""
         base = BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572")
         self.assertEqual(base.id, 'b6a6e15c-c67d-4312-9a75-9d084935e579')
-        self.assertEqual(base.created_at, datetime(2017, 9, 28, 21, 5, 54, 119572))
-        self.assertEqual(base.updated_at, datetime(2017, 9, 28, 21, 5, 54, 119572))
+  
+    def test_to_dict(self):
+        base = BaseModel()
+        self.assertEqual(type(base.to_dict()),dict)
+
+    def test__str__(self):
+        base = BaseModel(id= 'b6a6e15c-c67d-4312-9a75-9d084935e579', created_at="2017-09-28T21:05:54.119572", updated_at="2017-09-28T21:05:54.119572")
+        base_print = base.__str__()
+        self.assertEqual(base_print,"[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572), 'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572)}")
 
 if __name__ == "__main__":
     unittest.main()
